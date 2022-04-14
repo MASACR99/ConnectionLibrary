1c93e923aac1be85d3649b3494e0fd84e3d825be
Masa <joangil99@gmail.com>
2022-04-14 17:14:57 +0200

    Revert "Edited program to work with mobile connections. It should be used sending a packet 51 to initialize the player ball and then packet 50 to send movement commands"
    This reverts commit aa38332f10ff1c7d4ed1d0fb0be2f407856bbb43.

---
+++ Examples/balls/BallsPanel.java
@@ -0,0 +1,144 @@
+/*
+ * This project is given as is with license GNU/GPL-3.0. For more info look
+ * on github
+ */
+package balls;
+
+import static balls.HeyBalls.MAXBALLS;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * JPanel with the simulation and the different ways of controlling it via gui
+ * @author Joan Gil
+ */
+public class BallsPanel extends JPanel{
+    
+    private HeyBalls heyBalls;
+    private Simulation sim;
+    private JSpinner balls;
+    private boolean change = false;
+    
+    public BallsPanel(HeyBalls main){
+        super();
+        heyBalls = main;
+        construct();
+    }
+    
+    /**
+     * Fills the JPanel with stuff
+     */
+    public void construct(){
+        //Define everything related to the panel
+        GridBagConstraints c = new GridBagConstraints();
+        this.setLayout(new GridBagLayout());
+        
+        //Define the different buttons and thingies
+        sim = new Simulation(this.heyBalls,this);
+        JCheckBox gravity = new JCheckBox("Gravity?");
+        JCheckBox borders = new JCheckBox("Borders?");
+        JCheckBox dragg = new JCheckBox("Drag?");
+        
+        //Add change listener to keep gravity and border booleans as the checkboxes
+        //without having to check the checkboxes
+        gravity.addChangeListener(new ChangeListener(){
+            @Override
+            public void stateChanged(ChangeEvent e){
+                sim.setGrav(gravity.isSelected());
+            }
+        });
+        borders.addChangeListener(new ChangeListener(){
+            @Override
+            public void stateChanged(ChangeEvent e){
+                sim.setBorder(borders.isSelected());
+            }
+        });
+        dragg.addChangeListener(new ChangeListener(){
+            @Override
+            public void stateChanged(ChangeEvent e){
+                sim.setDrag(dragg.isSelected());
+            }
+        });
+        balls = new JSpinner(new SpinnerNumberModel(0,0,MAXBALLS,1));
+        balls.addChangeListener(new ChangeListener(){
+            @Override
+            public void stateChanged(ChangeEvent e){
+                if(!change){
+                    sim.setBalls((int)balls.getValue());
+                }else{
+                    change = false;
+                }
+            }
+        });
+        
+        //Attach everything together
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 4;
+        c.gridheight = 5;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        this.add(sim,c);
+        
+        c.gridx = 5;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.NONE;
+        c.weightx = 0;
+        c.weighty = 0;
+        this.add(gravity,c);
+        
+        c.gridx = 5;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        this.add(borders,c);
+        
+        c.gridx = 5;
+        c.gridy = 2;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        this.add(dragg,c);
+        
+        c.gridx = 5;
+        c.gridy = 4;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        this.add(balls,c);
+    }
+
+    /**
+     * Add a ball of object Ball to the simulation
+     * @param ball Object ball already created
+     */
+    void addBall(Ball ball) {
+        change = true;
+        balls.setValue((int)balls.getValue()+1);
+        sim.addBall(ball);
+    }
+    
+    /**
+     * Reduce by one the spinner value
+     */
+    public void minusOne(){
+        change = true;
+        balls.setValue((int)balls.getValue()-1);
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+        sim.paintComponent(g);
+    }
+    
+}
---
+++ Examples/balls/ScreenManager.java
@@ -0,0 +1,53 @@
+/*
+ * This project is given as is with license GNU/GPL-3.0. For more info look
+ * on github
+ */
+package balls;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+/**
+ * JPanel that has 2 panels on a tabbed pane
+ * @author Joan Gil
+ */
+public class ScreenManager extends JPanel{
+    
+    private BallsPanel jPanel1;
+    private ConnectionPanel jPanel2;
+    private HeyBalls heyBalls;
+    
+    public ScreenManager(HeyBalls heyBalls){
+        //Call super to also initialize default JPanel
+        super();
+        this.heyBalls = heyBalls;
+        jPanel1 = new BallsPanel(heyBalls);
+        jPanel2 = new ConnectionPanel(heyBalls);
+        JTabbedPane tabs = new JTabbedPane();
+        
+        //Finally set the final config
+        tabs.addTab("Balls", jPanel1);
+        tabs.addTab("Connections", jPanel2);
+        
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.BOTH;
+        c.weightx = 1;
+        c.weighty = 1;
+        this.add(tabs,c);
+    }
+    
+    /**
+     * Add a ball of object Ball to the simulation
+     * @param ball Already defined object Ball
+     */
+    public void addBall(Ball ball){
+        jPanel1.addBall(ball);
+    }
+    
+
+}
